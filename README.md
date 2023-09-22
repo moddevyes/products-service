28a44ec42083e71f5f00a7dec25dc3bd5484bb82
DavidKing <david.king@kinandcarta.com>
2023-09-22 18:06:48 -0500

    endpoints fwd via gateway, mysql connection and eureka

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # products-service
 Product Domain - Products Service
+
+## Local build and testing
+
+* Build, gradle clean build bootRun
+
+## DOCKER build and testing
+
+* Run script
+    - products-docker.sh
+
+- Pre-requisites
+
+1. Docker installation required, linux for script build with docker, docker-compose
+2. Normal gradle clean and build *before* running docker script
---
+++ products-docker.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# DATABASE
+echo ""
+echo "Creating PRODUCTS database."
+echo ""
+
+docker run --name products_mysql_container -d -p 3302:3307 -e MYSQL_ROOT_PASSWORD_FILE=/run/secrets/mysql-root-password \
+-e MYSQL_DATABASE=ecommerce_products_db \
+-e MYSQL_USER=davidking \
+-e MYSQL_PASSWORD=davidking!! \
+--env-file config/.env.dev -v ./secrets:/run/secrets --network commerce-net --restart unless-stopped mysql:8.0.1
+
+# DOCKER IMAGE
+echo ""
+echo "Building DOCKER image for products-service."
+echo ""
+docker build -t products-service .
+
+echo ""
+echo "Deploying/Running DOCKER image for products-service."
+echo ""
+docker run -d --env-file config/.env.dev --name=products_service_container --net=commerce-net -p 8002:8002 --restart unless-stopped products-service
+
+# VERIFY
+echo ""
+echo "VERIFY deployment"
+echo ""
+docker ps
+echo ""
+echo ""
+docker images
